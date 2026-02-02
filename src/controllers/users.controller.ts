1f3ef41f9cdcc180f833cfe60c9eb2f462cd0014
reginaldo <reginaldodarosacorreajr@gmail.com>
2026-02-02 15:26:22 -0300

    routes and prisma

---
+++ src/controllers/users.controller.ts
@@ -0,0 +1,23 @@
+
+import { Request, Response } from "express"
+import z from 'zod'
+import { hash } from 'bcrypt'
+
+
+class UsersController {
+    async create(request: Request, response: Response){
+        const bodySchema = z.object({
+            name: z.string(),
+            email: z.string().email(),
+            password: z.string().min(6)
+        })
+
+        const {name, email, password} = bodySchema.parse(request.body)
+
+        const hashedPassword = await hash(password, 8)
+        console.log(name, email, hashedPassword)
+        return response.json({message: "ok"})
+    }
+}
+
+export {UsersController}
